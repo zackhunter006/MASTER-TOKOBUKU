9172f6d15d387c31a5087f20af862013abd0e8e6
zackhunter006 <48428693+zackhunter006@users.noreply.github.com>
2019-07-29 13:42:07 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,8 @@
+<?php
+//$koneksi1 = mysqli_connect("localhost","root","","food");
+$koneksi1 = mysqli_connect("localhost","id10325621_zakyfaishal",'nasigoreng',"id10325621_tokbuku");
+// Check connection
+if (mysqli_connect_errno()){
+	echo "Koneksi database gagal : " . mysqli_connect_error();
+}
+?>
